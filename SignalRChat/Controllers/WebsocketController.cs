6e92a0e2f797c3053d045258e10369283f1510b8
jpedr <jpedr@JOAOPEDRO>
2023-08-31 11:57:46 -0300

    Add project files.

---
+++ SignalRChat/Controllers/WebsocketController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+
+namespace SignalRChat.Controllers
+{
+    public class WebsocketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private static async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!receiveResult.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(
+                    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                    receiveResult.MessageType,
+                    receiveResult.EndOfMessage,
+                    CancellationToken.None);
+
+                receiveResult = await webSocket.ReceiveAsync(
+                    new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await webSocket.CloseAsync(
+                receiveResult.CloseStatus.Value,
+                receiveResult.CloseStatusDescription,
+                CancellationToken.None);
+        }
+    }
+}
